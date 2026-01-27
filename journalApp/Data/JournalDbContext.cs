379a9b7b10af777258f8691afe9196b58fdc6146
nirdeshbkh <kneerdeshchrest@gmail.com>
2026-01-27 16:41:54 +0545

    Save local work before merge

---
+++ journalApp/Data/JournalDbContext.cs
@@ -0,0 +1,27 @@
+﻿using JournalApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JournalApp.Data;
+
+public class JournalDbContext : DbContext
+{
+    public JournalDbContext(DbContextOptions<JournalDbContext> options) : base(options) { }
+
+    public DbSet<JournalEntry> JournalEntries => Set<JournalEntry>();
+    public DbSet<Tag> Tags => Set<Tag>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Ensure "one journal entry per day"
+        modelBuilder.Entity<JournalEntry>()
+            .HasIndex(e => e.EntryDate)
+            .IsUnique();
+
+        // Many-to-many (EF Core will create join table automatically)
+        modelBuilder.Entity<JournalEntry>()
+            .HasMany(e => e.Tags)
+            .WithMany(t => t.JournalEntries);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ journalApp/Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JournalApp.Models;
+
+public class Tag
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    public List<JournalEntry> JournalEntries { get; set; } = new();
+}
